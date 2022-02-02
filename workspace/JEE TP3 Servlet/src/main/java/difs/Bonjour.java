38ea3ce645e55fc790a467e4a2bc9a2c3bd64cb1
2Guib <maxime.deguibert@orange.fr>
2022-02-02 08:40:47 +0100

    JEE-TP3 Premier servlet Bonjour

---
+++ workspace/JEE TP3 Servlet/src/main/java/difs/Bonjour.java
@@ -0,0 +1,22 @@
+package difs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Bonjour extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("Salut");
+		resp.setStatus(HttpServletResponse.SC_OK);
+	}
+}
